d2f0a09c7f969895c165f3f89e739607a8e455ae
LomNtetha <lumkilentetha878@gmail.com>
2023-04-23 20:03:40 +0200

    adiing sign page that came from IAM

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 
 1. Clone the project
 ```
-git clone https://github.com/A4ANK/Fake-Product-Identification.git
+https://github.com/LomNtetha/blockchain-fakeID.git
 ```
 2. Go to the project folder, open terminal there and run following command to install required node_modules:-
 ```
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,61 @@
+// (function($) {
+
+// 	"use strict";
+
+// 	$('nav .dropdown').hover(function(){
+// 		var $this = $(this);
+// 		$this.addClass('show');
+// 		$this.find('> a').attr('aria-expanded', true);
+// 		$this.find('.dropdown-menu').addClass('show');
+// 	}, function(){
+// 		var $this = $(this);
+// 			$this.removeClass('show');
+// 			$this.find('> a').attr('aria-expanded', false);
+// 			$this.find('.dropdown-menu').removeClass('show');
+// 	});
+
+// })(jQuery);
+
+// const accessToken = window.localStorage.getItem("accessToken");
+// const refreshToken = window.localStorage.getItem("refreshToken");
+
+// const resultElement = document.getElementById("getResult");
+// resultElement.innerHTML =
+//   "<pre>" + JSON.stringify({ accessToken, refreshToken }, null, 2) + "</pre>";
+// // 
+
+
 (function($) {
-
 	"use strict";
-
+  
 	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
+	  var $this = $(this);
+	  $this.addClass('show');
+	  $this.find('> a').attr('aria-expanded', true);
+	  $this.find('.dropdown-menu').addClass('show');
 	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
+	  var $this = $(this);
+		$this.removeClass('show');
+		$this.find('> a').attr('aria-expanded', false);
+		$this.find('.dropdown-menu').removeClass('show');
 	});
+  })(jQuery);
+  
+//   const accessToken = window.localStorage.getItem("accessToken");
+//   const refreshToken = window.localStorage.getItem("refreshToken");
+  
+//   const resultElement = document.getElementById("getResult");
+//   resultElement.innerHTML =
+// 	"<pre>" + JSON.stringify({ accessToken, refreshToken }, null, 2) + "</pre>";
+  
+const accessToken = window.localStorage.getItem("accessToken");
+const refreshToken = window.localStorage.getItem("refreshToken");
 
-})(jQuery);
+if (!accessToken || !refreshToken) {
+  // redirect to signin page
+  window.location.href = "signin.html";
+}
+
+const resultElement = document.getElementById("getResult");
+resultElement.innerHTML =
+  "<pre>" + JSON.stringify({ accessToken, refreshToken }, null, 2) + "</pre>";
